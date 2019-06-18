4a07d562baabb3221f7899bf725e41531ca69978
kingtamaaj <kingtamaaj@gmail.com>
2019-06-17 21:40:00 -0400

    Finished mvp

--- index.js
+++ index.js
@@ -27,13 +27,94 @@ server.get('/users', (req, res) => {
         .catch(err => {
             res.status(500).json({
                 success: false,
-                err
+                err: 'The user information could not be retrieved.',
             });
         })
 })
 
+// POST
+server.post('/users', (req, res) => {
+    const  userInfo = req.body; 
+    console.log('req body', req.body);
+    
+    db.add(userInfo)
+        .then(user => {
+            res.status(201).json({
+                success: true, user});
+        })
+        .catch(err => {
+            res.status(500).json({
+                success: false,
+                err: 'The user information could not be retrieved.',
+            });
+        });
+})
 
+// DELETE
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
 
+        db.remove(id)
+            .then(deleted => {
+                if (deleted) {
+                    res.status(204).end();
+                } else {
+                    res.status(404).json({
+                        success: false,
+                        message: 'The user with the specified ID does not exist.'
+                    });
+                }
+            })
+                .catch(err => {
+                    res.status(500).json({
+                        success: false,
+                        err
+                    })
+                })
+})
+
+// PUT
+server.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db.update(id, changes)
+      .then(updated => {
+          if (updated) {
+              res.status(200).json({
+                  success: true, updated });
+          } else {
+              res.status(404).json({
+                  success: false,
+                  message: 'The user with the specified ID does not exist.',
+              });
+          }
+      })
+      .catch(err => {
+          res.status(500).json({
+              success: false,
+              err,
+          });
+      });
+});
+
+// GET by ID
+server.get('/users/:id', (req, res) => {
+    db.findById(req.params.id)
+        .find(user => {
+            if (user) {
+                res.status(200).json({
+                    success: true,
+                    user,
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'The user with the specified ID does not exist.'
+                })
+            }
+        });
+})
 
 server.listen(4000, () => {
     console.log('\n*** Server Running on http://localhost:4000 ***\n')
